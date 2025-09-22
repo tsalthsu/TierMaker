6cd63378176f4ed984a3aebb2af75d88cdca32a7
Tsalthsu <165044376+tsalthsu@users.noreply.github.com>
2025-09-22 14:02:52 +0900

    Update ops5.js

--- api/ops5.js
+++ api/ops5.js
@@ -80,7 +80,7 @@ export default async function handler() {
       list.push({
         id: key,
         label,
-        image: prtsIcon(nameCN),
+        image: '/api/img?url=' + encodeURIComponent(prtsIcon(nameCN)),
       });
     }
 
